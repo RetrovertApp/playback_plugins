96235c50864ebe787662bca1bc3bbc4816160e28
Daniel Collin <daniel@collin.com>
2026-03-15 19:52:25 +0100

    Extract furnace, libkss, libvgm, sc68, sidplayfp, uade, xsf, zxtune to separate repos

--- uade/uade/compat/strnlen.h
+++
@@ -1,8 +0,0 @@
-#ifndef _UADE_STRNLEN_H_
-#define _UADE_STRNLEN_H_
-
-#include <stddef.h>
-
-size_t strnlen(const char *s, size_t len);
-
-#endif
--- uade/uade/compat/strsep.h
+++
@@ -1,6 +0,0 @@
-#ifndef _UADE_STRSEP_H_
-#define _UADE_STRSEP_H_
-
-char *strsep(char **stringp, const char *delim);
-
-#endif
